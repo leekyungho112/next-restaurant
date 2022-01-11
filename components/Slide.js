27f889c8b0173460fd2aec7d3f116c9fbf7936d0
kyungho <epsoe@naver.com>
2022-01-11 15:43:23 +0900

    Slide done

--- components/Slide.js
+++ components/Slide.js
@@ -22,6 +22,7 @@ const style = css`
     width: 300vw;
     height: 100%;
     display: flex;
+    transition: all 1s ease-in-out;
   }
   .imgContainer {
     width: 100vw;
@@ -32,6 +33,16 @@ const style = css`
 
 const Slide = () => {
   const [index, setIndex] = useState(0);
+
+  const handleArrow = (direction) => {
+    if (direction === 'left') {
+      setIndex((prev) => (index !== 0 ? prev - 1 : 2));
+    }
+    if (direction === 'right') {
+      setIndex((prev) => (index !== 2 ? prev + 1 : 0));
+    }
+  };
+
   const images = [
     '/img/featured.png',
     '/img/featured2.png',
@@ -39,18 +50,39 @@ const Slide = () => {
   ];
   return (
     <div className="container">
-      <div className="arrowContainer" style={{ left: 0 }}>
-        <Image src="/img/arrowleft.png" alt="" layout="fill" />
+      <div
+        className="arrowContainer"
+        onClick={() => handleArrow('left')}
+        style={{ left: 0 }}
+      >
+        <Image
+          src="/img/arrowleft.png"
+          alt=""
+          layout="fill"
+          objectFit="contain"
+        />
       </div>
-      <div className="wrapper">
+      <div
+        className="wrapper"
+        style={{ transform: `translateX(${-100 * index}vw)` }}
+      >
         {images.map((img, i) => (
           <div className="imgContainer" key={i}>
             <Image src={img} alt="" layout="fill" objectFit="contain" />
           </div>
         ))}
       </div>
-      <div className="arrowContainer" style={{ right: 0 }}>
-        <Image src="/img/arrowright.png" alt="" layout="fill" />
+      <div
+        className="arrowContainer"
+        onClick={() => handleArrow('right')}
+        style={{ right: 0 }}
+      >
+        <Image
+          src="/img/arrowright.png"
+          alt=""
+          layout="fill"
+          objectFit="contain"
+        />
       </div>
       <style jsx>{style}</style>
     </div>
